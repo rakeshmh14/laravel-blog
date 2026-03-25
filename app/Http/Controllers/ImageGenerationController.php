f250bb5cd579a4a453759fc6597fab1e9f4fd43b
Rakesh Maharjan <rakeshmh14@gmail.com>
2026-03-25 16:07:01 +0545

    working with openAI api to generate image

--- app/Http/Controllers/ImageGenerationController.php
+++ app/Http/Controllers/ImageGenerationController.php
@@ -2,17 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\GeneratePromptRequest;
 use Illuminate\Http\Request;
+use OpenAI;
 
 class ImageGenerationController extends Controller
 {
+    public function __construct(private OpenAiService $openAiService)
+    {
+        throw new \Exception('Not implemented');
+    }
+
     public function index()
     {
 
     }
 
-    public function store(Request $request)
+    public function store(GeneratePromptRequest $request)
     {
-        
+        $user = $request->user();
+        $image = $request->file('image');
+
+        $originalName = $image->getClientOriginalName();
+        $sanitizedName = preg_replace('/[^a-zA-Z0-9', '_', pathinfo($originalName, PATHINFO_FILENAME));
+        $extension = $image->guessClientExtension();
+        $safeFilename = $sanitizedName. '_'.Str::random(10) . $extension;
+
+        $image->storAs('uploads/images',$safeFilename, 'public');
+
+        $
+
     }
 }
+    
